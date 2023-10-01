de8f0f3c715ecffab5e7b0c78b01953323763812
Sreeraksha <raksharamesh1998@gmail.com>
2023-10-01 23:08:40 +0530

    typescript changes for section title component

--- src/components/blocks/section.title.tsx
+++ src/components/blocks/section.title.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import section from '../../../assets/styles/scss/blocks/section.title.module.scss';
 
 interface SectionTitleProps {
-	preTitle: string;
-	title: string;
+	preTitle?: string;
+	title?: string;
 	subTitle?: string;
 }
 
 const SectionTitle: React.FC<SectionTitleProps> = ({ preTitle, title, subTitle }) => {
 	return (
 		<div className={`${section.title}`}>
-			<h4>{preTitle}</h4>
-			<h2>{title}</h2>
+			{preTitle ? <h4>{preTitle}</h4> : ''}
+			{title ? <h2>{title}</h2> : ""}  
 			{subTitle ? <p className="subtitle">{subTitle}</p> : ''}
 		</div>
 	);
