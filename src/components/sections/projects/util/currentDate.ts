5981a99e02cf521fadfb7a58ad117d2ca84ae9a6
Ankith G <corra-337@Corra-337.local>
2023-10-03 16:56:26 +0530

    main: Error handling for typescript

--- src/components/sections/projects/util/currentDate.ts
+++ src/components/sections/projects/util/currentDate.ts
@@ -1,5 +1,6 @@
-export function getFormattedCurrentDate() {
-    const currentDate = new Date();
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+export function getFormattedCurrentDate(): string {
+    const currentDate: Date = new Date();
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
     return currentDate.toLocaleDateString('en-US', options);
   }
+  
